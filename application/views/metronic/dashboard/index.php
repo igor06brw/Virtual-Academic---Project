f08de43f914d456e3ebc704a04263db0275eb8f8
Mateusz Rybicki <mr@netforge.pl>
2016-03-21 14:56:24 +0100

    PHPSTORM commit7

--- application/views/metronic/dashboard/index.php
+++ application/views/metronic/dashboard/index.php
@@ -45,7 +45,7 @@
 							<div class="portlet-body">
                               
 							  
-							  <p>Hello world6</p>
+							  <p>Hello world - changes</p>
                     
                             
                             </div>
