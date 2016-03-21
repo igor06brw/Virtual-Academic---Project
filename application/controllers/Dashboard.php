ca777ccd646e3cb5556550dd35c80460e3516606
Mateusz Rybicki <mr@netforge.pl>
2016-03-21 13:23:28 +0100

    Test from PHPSTORM2

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -16,7 +16,7 @@ class Dashboard extends CI_Controller {
                 $data['smallTitle'] = '';
                 $data['packages'] = $this->plugins->get(array('Pulsate','FormStuff','dhtmlXPro','Typehead'));
                 $this->load->template('dashboard/index' ,$data);
-
+                echo 'test';
 
 
         }
