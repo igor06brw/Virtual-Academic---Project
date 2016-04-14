eba55467945b96a381e58d8a9264a3ed2dcc7d87
Mateusz Rybicki <mr@netforge.pl>
2016-04-15 00:13:34 +0200

    update database config

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => '185.46.170.73',
 	'username' => 'projectx',
 	'password' => 'iLyj29?1',
 	'database' => 'admin_projectx',
